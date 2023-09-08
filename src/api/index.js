7816b1f70f3c0025c674012647bcc17b83148ef8
Anna <gstudentseo@gmail.com>
2023-09-08 23:38:19 +0200

    API connection

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import API_KEY from '../../config';
+
+const URL =
+  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
